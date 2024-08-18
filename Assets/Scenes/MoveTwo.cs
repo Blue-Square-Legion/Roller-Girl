f149bf463722b0f78ce7b12796fe46dcf73a3ff8
Robert Chaney <rwchaneyjr@yahoo.com>
2024-08-18 16:31:56 -0500

    basic mechanics
    Movement with acceleration and jumping is implemented in robert-branch

---
+++ Assets/Scenes/MoveTwo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MoveTwo : MonoBehaviour
+{
+    float horizontal;
+float speed = 8f;
+float jumpPower = 5f;
+bool isFacingRight = true;
+float acceleration = 2;
+[SerializeField] private Rigidbody2D rb;
+[SerializeField] private Transform groundCheck;
+[SerializeField] private LayerMask groundLayer;
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    
+if (Input.GetKey(KeyCode.Space))
+{
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isFacingRight)  
+        rb.velocity = new Vector2(horizontal * speed * acceleration, rb.velocity.y);
+        else
+         rb.velocity = new Vector2(horizontal * speed , rb.velocity.y);
+        if(!isFacingRight)
+            rb.velocity = new Vector2(horizontal * speed * acceleration, rb.velocity.y);
+        else
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+}
