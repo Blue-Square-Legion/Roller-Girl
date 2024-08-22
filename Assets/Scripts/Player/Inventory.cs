ce84abc40ca2054dce59ca92393bc52994a3e27d
Robert Chaney <rwchaneyjr@yahoo.com>
2024-08-22 14:34:11 -0500

    inventory
    There is a translucent orange and green box. The orange box when the player collides or enters and presses M the number of Items increments(it happens fast) when the player enters the green box and presses L the number of items is subtracted (it happens kind of fast)

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+public class Inventory : MonoBehaviour
+{
+
+    public TextMeshProUGUI ItemsText;
+    float num = 0;
+    bool recieve = false;
+    bool give = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!ItemsText)
+            ItemsText = GetComponent<TextMeshProUGUI>();
+
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("recieve"))
+        {
+            recieve = true;
+            give = false;
+        }
+        if (other.gameObject.CompareTag("give"))
+        {
+            recieve = false;
+            give = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("recieve"))
+        {
+            recieve =false;
+            
+        }
+        if (other.gameObject.CompareTag("give"))
+        {
+           
+            give = false;
+        }
+
+    }
+
+    void Items(float num)
+    {
+
+      
+        ItemsText.text = num.ToString();
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.M) && recieve)
+        {
+            num = num + 1;
+
+            Debug.Log("M");
+        }
+        Items(num);
+        if (Input.GetKey(KeyCode.L) && give)
+        {
+
+            Debug.Log(num);
+            Debug.Log(ItemsText);
+            num =num - 1;
+
+            Debug.Log("L");
+        }
+        Items(num);
+
+    }
+}
