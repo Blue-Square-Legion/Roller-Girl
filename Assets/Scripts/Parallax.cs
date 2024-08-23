5b8ad9f978eaa006996bcc5eb9f19cf53f39ca11
K3470N <144397362+K3470N@users.noreply.github.com>
2024-08-23 14:00:50 -0500

    Parallax Start
    Created scene (Parallax_Demo), added background images and "Parallax" script to create parallax background effect

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float ParallaxEffect;
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - ParallaxEffect));
+        float dist = (cam.transform.position.x * ParallaxEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
