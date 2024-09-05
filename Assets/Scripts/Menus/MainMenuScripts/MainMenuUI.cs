4c030693f2e999ea3a81d602a16c717632936e5f
Eman5677 <edorsey4@masonlive.gmu.edu>
2024-09-05 16:39:51 -0400

    controlscreen exist now

--- Assets/Scripts/Menus/MainMenuScripts/MainMenuUI.cs
+++ Assets/Scripts/Menus/MainMenuScripts/MainMenuUI.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.Audio;
+using System;
 
 public class MainMenuUI : MonoBehaviour
 {
@@ -19,13 +20,14 @@ public class MainMenuUI : MonoBehaviour
     private Button settingsBackButton;
 
     private Button audioOptionsBackButton;
+    private Button controlsScreenBackButton;
 
     private VisualElement titleScreen;
     private VisualElement mainMenu;
     private VisualElement settingsScreen;
     private VisualElement audioOptions;
     public GameObject optionsScreen;
-    //private VisualElement controlsScreen;
+    private VisualElement controlsScreen;
     //private VisualElement displayOptions;
 
     void Start()
@@ -39,10 +41,11 @@ public class MainMenuUI : MonoBehaviour
         mainMenu = root.Q<VisualElement>("MainMenu");
         settingsScreen = root.Q<VisualElement>("SettingsScreen");
         audioOptions = root.Q<VisualElement>("AudioOptions");
+        controlsScreen = root.Q<VisualElement>("ControlsScreen");
 
         playButton = root.Q<Button>("PlayButton");
         settingsButton = root.Q<Button>("SettingsButton");
-        controlsButton = root.Q<Button>("ContolsButton");
+        controlsButton = root.Q<Button>("ControlsButton");
         quitButton = root.Q<Button>("QuitButton");
 
         displayButton = root.Q<Button>("DisplayButton");
@@ -50,12 +53,13 @@ public class MainMenuUI : MonoBehaviour
         settingsBackButton = root.Q<Button>("SettingsBackButton");
 
         audioOptionsBackButton = root.Q<Button>("AudioOptionsBackButton");
+        controlsScreenBackButton = root.Q<Button>("ControlsScreenBackButton");
 
         presstoStartButton.RegisterCallback<ClickEvent>(OnPresstoStartButtonClicked);
 
         playButton.RegisterCallback<ClickEvent>(OnPlayButtonClicked);
         settingsButton.RegisterCallback<ClickEvent>(OnSettingsButtonClicked);
-        //controlsButton.RegisterCallback<ClickEvent>(OnControlsButtonClicked);
+        controlsButton.RegisterCallback<ClickEvent>(OnControlsButtonClicked);
         quitButton.RegisterCallback<ClickEvent>(OnQuitButtonClicked);
 
         //displayButton.RegisterCallback<ClickEvent>(OnDisplayButtonClicked);
@@ -63,38 +67,18 @@ public class MainMenuUI : MonoBehaviour
         settingsBackButton.RegisterCallback<ClickEvent>(OnSettingsBackButtonClicked);
 
         audioOptionsBackButton.RegisterCallback<ClickEvent>(OnAudioOptionsBackButtonClicked);
+        controlsScreenBackButton.RegisterCallback<ClickEvent>(OnControlScreenBackButtonClicked);
 
         titleScreen.style.display = DisplayStyle.Flex;
         mainMenu.style.display = DisplayStyle.None;
         settingsScreen.style.display = DisplayStyle.None;
         audioOptions.style.display = DisplayStyle.None;
-
-
-        //contolScreen.style.display = DisplayStyle.None;
+        controlsScreen.style.display = DisplayStyle.None;
         //displayOptions.style.display = DisplayStyle.None;
-
-        /*
-        mastLabel = root.Q<Label>("MasterVol");
-        musicLabel = root.Q<Label>("MusicVol");
-        sfxLabel = root.Q<Label>("SFXVol");
-
-        mastSlider = root.Q<Slider>("MasterSlider");
-        musicSlider = root.Q<Slider>("MusicSlider");
-        sfxSlider = root.Q<Slider>("SFXSlider");
-
-        float vol = 0f;
-        theMixer.GetFloat("", out vol);
-        mastSlider.value = vol;
-        theMixer.GetFloat("", out vol);
-        musicSlider.value = vol;
-        theMixer.GetFloat("", out vol);
-        sfxSlider.value = vol;
-
-        mastLabel.text = Mathf.RoundToInt(mastSlider.value + 80).ToString();
-        musicLabel.text = Mathf.RoundToInt(musicSlider.value + 80).ToString();
-        sfxLabel.text = Mathf.RoundToInt(sfxSlider.value + 80).ToString();
-        */
     }
+
+    
+
     private void OnPresstoStartButtonClicked(ClickEvent clickEvent)
     {
         titleScreen.style.display = DisplayStyle.None;
@@ -103,7 +87,7 @@ public class MainMenuUI : MonoBehaviour
 
     private void OnPlayButtonClicked(ClickEvent clickEvent) { SceneManager.LoadScene("Main_Demo", LoadSceneMode.Single); }
     private void OnSettingsButtonClicked(ClickEvent clickEvent) { settingsScreen.style.display = DisplayStyle.Flex; mainMenu.style.display = DisplayStyle.None; }
-    //private void OnControlsButtonClicked(ClickEvent clickEvent) { /*controlsScreen.style.display = DisplayStyle.None; */}
+    private void OnControlsButtonClicked(ClickEvent clickEvent) { controlsScreen.style.display = DisplayStyle.Flex; }
     private void OnQuitButtonClicked(ClickEvent clickEvent) { Application.Quit(); }
 
     //private void OnDisplayButtonClicked(ClickEvent clickEvent) { /*displayOptions.style.display = DisplayStyle.Flex; */}
@@ -123,5 +107,10 @@ public class MainMenuUI : MonoBehaviour
         optionsScreen.SetActive(false); 
         settingsScreen.style.display = DisplayStyle.Flex; 
         audioOptions.style.display = DisplayStyle.None; 
+    }
+    private void OnControlScreenBackButtonClicked(ClickEvent clickEvent)
+    {
+        controlsScreen.style.display = DisplayStyle.None;
+        settingsScreen.style.display = DisplayStyle.Flex;
     }
 }
